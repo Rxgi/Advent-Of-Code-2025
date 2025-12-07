7b5179617251d431898af38444ed2041f8d9b477
Reagan Dunning <dunningreagan@outlook.com>
2025-12-07 08:43:26 +0000

    adding files to repo

---
+++ Day 1/Secret Entrance.py
@@ -0,0 +1,14 @@
+dial = range(100)
+
+def sub(x,y):
+    x-y
+
+def add(x,y):
+    x+y
+
+
+def fullRotation(rotationCode : str) -> int:
+    if "R" in rotationCode:
+        add()
+    else:
+        sub()
