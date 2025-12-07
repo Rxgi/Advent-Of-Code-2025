12c258742dab4ddf2417837cf46b9eb974ae7239
Reagan Dunning <dunningreagan@outlook.com>
2025-12-07 09:27:46 +0000

    started day 1

--- Day 01/Secret Entrance.py
+++ Day 01/Secret Entrance.py
@@ -1,14 +1,15 @@
-dial = range(100)
+DIAL_RANGE = range(100)
+dial_start = 50
 
 def sub(x,y):
-    x-y
+    z = x + y
+    return(z)
 
 def add(x,y):
-    x+y
+    z = x + y
+    return(z)
 
 
-def fullRotation(rotationCode : str) -> int:
-    if "R" in rotationCode:
-        add()
-    else:
-        sub()
+def check_rotation(code):
+    if code[0] == "L":
+        
