3c1d8fd401d6c7362cd5ebf3669853e136fdadfc
Reagan Dunning <dunningreagan@outlook.com>
2025-12-08 18:24:28 +0000

    08/12/25 18:00pm - continuing to work on secret entrance puzzle

--- Day 01/Secret Entrance.py
+++ Day 01/Secret Entrance.py
@@ -4,5 +4,13 @@ dial_start = 50
 test_input = "R11"
 
 rotation = test_input[0]
-rotation_ammount = int(test_input[1:])
+rotation_amount = int(test_input[1:])
+
+def check_rotation(pos, rotation, amount):
+    if rotation == "R":
+        return (pos + amount) % 100
+    else:
+        return (pos - amount) % 100
+
+
 
